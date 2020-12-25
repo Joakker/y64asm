6d14fa46ad6eb8e78d31ccc8fcb81b97d8e373d4
Joakker <joaquinandresleon108@gmail.com>
2020-12-24 21:14:19 -0300

    Implemented a basic tree listener for the grammar.

--- main.go
+++ main.go
@@ -9,39 +9,79 @@ import (
 
 type AsmListener struct {
 	*parser.BaseY64asmListener
-	currAddress uint
-	labels      map[string]uint
+	currAddr uint
+	labels   map[string]uint
 }
 
 func NewAsmListener() *AsmListener {
 	return &AsmListener{
-		currAddress: 0,
-		labels:      map[string]uint{},
+		currAddr: 0,
+		labels:   map[string]uint{},
 	}
 }
 
+func (l *AsmListener) EnterPreproc(c *parser.PreprocContext) {
+	fmt.Printf("Preprocessing directive: %s, operand %s\n",
+		c.GetStart().GetText(), c.GetStop().GetText())
+}
+
 func (l *AsmListener) EnterLabel(c *parser.LabelContext) {
-	l.labels[c.ID().GetText()] = l.currAddress
+	l.labels[c.ID().GetText()] = l.currAddr
+	fmt.Println("Label: ", c.ID().GetText())
 }
 
-func (l *AsmListener) ExitInstr0(c *parser.Instr0Context) {
-	l.currAddress++
+func (l *AsmListener) EnterInstr2(c *parser.Instr2Context) {
+	switch c.GetStart().GetTokenType() {
+	case parser.Y64asmLexerOP:
+		op := c.GetStart().GetText()
+		fmt.Printf("%s between registers: %s\n",
+			op, c.GetTokens(parser.Y64asmLexerREG))
+	case parser.Y64asmLexerIOP:
+		op := c.GetStart().GetText()[1:]
+		k, _ := c.GetChild(1).(*parser.ReferContext)
+		fmt.Printf("%s number %s into register %s\n",
+			op, k.GetText(), c.GetStop().GetText(),
+		)
+	}
 }
 
-func (l *AsmListener) ExitInstr1(c *parser.Instr1Context) {
-	l.currAddress += 2
+func (l *AsmListener) EnterInstr1(c *parser.Instr1Context) {
+	switch c.GetStart().GetTokenType() {
+	case parser.Y64asmLexerCALL:
+		fmt.Printf("Calling function: %s\n", c.GetStop().GetText())
+	case parser.Y64asmLexerJMP:
+		fmt.Printf("Jumping to: %s\n", c.GetStop().GetText())
+	}
 }
 
-func (l *AsmListener) ExitInstr2(c *parser.Instr2Context) {
-	l.currAddress += 3
+func (l *AsmListener) EnterInstr0(c *parser.Instr0Context) {
+	switch c.GetStart().GetTokenType() {
+	case parser.Y64asmLexerRET:
+		fmt.Println("Returning")
+	case parser.Y64asmLexerHALT:
+		fmt.Println("HALT")
+	}
 }
+
+func (l *AsmListener) ExitInstr0(c *parser.Instr0Context) { l.currAddr++ }
+
+func (l *AsmListener) ExitInstr1(c *parser.Instr1Context) { l.currAddr += 2 }
+
+func (l *AsmListener) ExitInstr2(c *parser.Instr2Context) { l.currAddr += 3 }
 
 func main() {
 	is := antlr.NewInputStream(`
+.pos 0
+	jmp		label1
 label1:
-	irmovq $10, %r10
-	irmovq 0x10, %r9
-laber2:
+	irmovq	$10, %r10
+	irmovq	0x10, %r9
+	add		%r9, %r10
+	call	label2
+	halt
+label2:
+	iadd	0x5, %r10
+	ret
 `)
 	lexer := parser.NewY64asmLexer(is)
 	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
